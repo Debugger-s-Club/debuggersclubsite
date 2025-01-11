775585f5dcdb295b53b0763bc6d6017b3c65759c
Tejas <tejasnanoti2@gmail.com>
2025-01-11 22:41:54 +0530

    fix: Fix Tailwind config;

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,25 +1,19 @@
 // tailwind.config.js
 
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily:{
-        'inter':['Inter','sans-serif'],
-      },
-      screen:{
-        'xs':'250px',
-      },
-      fontSize: {
-        '10xl': '10rem', // Custom text size
-        '11xl': '11rem', // Additional custom text size if needed
-        // Add more custom text sizes as needed
-      },
-      
+export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"];
+export const theme = {
+  extend: {
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+    },
+    screen: {
+      xs: "250px",
+    },
+    fontSize: {
+      "10xl": "10rem", // Custom text size
+      "11xl": "11rem", // Additional custom text size if needed
+      // Add more custom text sizes as needed
     },
   },
-  plugins: [],
-}
+};
+export const plugins = [];
